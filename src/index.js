4dc0b4105022597463dd4341fead9b2afaef2d6d
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-09-01 20:11:28 +0900

    just use prettier

--- src/index.js
+++ src/index.js
@@ -12,4 +12,3 @@ const init = async () => {
 };
 
 init();
-
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,8 +1,10 @@
 import { h } from 'preact';
 import { Page } from '@components/page';
 
-const HomePage = () => (<Page>
-  <h1>Home</h1>
-</Page>);
+const HomePage = () => (
+  <Page>
+    <h1>Home</h1>
+  </Page>
+);
 
 export default HomePage;
